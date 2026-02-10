982787f9649df561a3a5e6f7fd9b0871343a28ad
Nurul Jannah Hasnaa <211233@student.upm.edu.my>
2026-02-10 23:46:35 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const urlDB = mysql.createConnection(process.env.MYSQL_URL);
-
-const db = mysql.createConnection(
-  urlDB
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME}
 );
 
 db.connect((err) => {
