ce90354cca0f53012b77d0033d3c182d50f76bf4
Nurul Jannah Hasnaa <211233@student.upm.edu.my>
2026-02-10 23:30:17 +0800

    Update db.js to use MYSQL_URL and dotenv

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const urlDB = 'mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:$process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}'
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+
 const db = mysql.createConnection(
   urlDB
 );
